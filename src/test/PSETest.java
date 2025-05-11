d92662f4b53c2a39a2838348a364235115334423
jimfeserHTW <s0596491@htw-berlin.de>
2025-05-11 18:42:58 +0200

    Test Tested

--- src/test/PSETest.java
+++ src/test/PSETest.java
@@ -1,54 +1,23 @@
 package test;
 
 import org.junit.Test;
+
+import main.data.PeriodicTable;
+
 import static org.junit.Assert.*;
+
+import java.awt.Color;
+import java.util.Scanner;
 
 // Nur AssertEquals wird im Bash Skript berücksichtigt
 
 public class PSETest {
 
     @Test
-    public void testSomething() {
-        assertEquals(2, 1);
+    public void testGetElementColor() {
+        Color color = PeriodicTable.getElementColor("Mg");
+
+        assertEquals(new Color(152, 173, 121), color);
     }
 
-    @Test
-    public void tesstSomething() {
-        assertEquals(546546451, 324324321);
-    }
-
-    @Test
-    public void tesstsSomething() {
-        assertEquals(1324324, 234324322);
-    }
-
-    @Test
-    public void tesstsSomehing() {
-        assertEquals(1323, 2);
-    }
-
-    @Test
-    public void tesstswSomehing() {
-        assertEquals(1323, 6456452);
-    }
-
-    @Test
-    public void tesswtsSomehing() {
-        assertEquals(1323, 56456542);
-    }
-
-    @Test
-    public void tesstscSomehing() {
-        assertEquals(1323, 2);
-    }
-
-    @Test
-    public void tesstsSaomehing() {
-        assertEquals(1323, 2);
-    }
-
-    @Test
-    public void tesstsdSomehing() {
-        assertEquals(1323, 54654652);
-    }
 }
