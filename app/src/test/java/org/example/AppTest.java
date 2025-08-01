bf1b0a74d07b927ec095ec36c8deff59649a9176
jimfeserHTW <s0596491@htw-berlin.de>
2025-08-01 17:16:36 +0200

    github workflow update

--- app/src/test/java/org/example/AppTest.java
+++ app/src/test/java/org/example/AppTest.java
@@ -6,9 +6,11 @@ package org.example;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
+import start.App;
+
 class AppTest {
-    @Test void appHasAGreeting() {
+    @Test void appInitialises() {
         App classUnderTest = new App();
-        assertNotNull(classUnderTest.getGreeting(), "app should have a greeting");
+        assertNotNull(classUnderTest);
     }
 }
